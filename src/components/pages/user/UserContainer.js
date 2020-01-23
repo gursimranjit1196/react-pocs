1cebaeba6d4ef117e37d07ad737c1a2dbb4bacb7
Gursimranjit Singh <gursimranjit.singh@metadesignsolutions.co.uk>
2020-01-23 10:38:34 +0530

    minor changes

--- src/components/pages/user/UserContainer.js
+++ src/components/pages/user/UserContainer.js
@@ -47,7 +47,7 @@ class UserContainer extends React.Component {
     return (
         <form>
             <CustomInput type = { this.state.userForm.name.type } elementConfig = { this.state.userForm.name.elementConfig } value = { this.state.userForm.name.value } valueChangeHandler = { this.valueChangeHandler } label = { this.state.userForm.name.label } name = { this.state.userForm.name.name }  />
-            <CustomSelect type = { this.state.userForm.gender.type } elementConfig = { this.state.userForm.gender.elementConfig } value = { this.state.userForm.gender.value } valueChangeHandler = { this.valueChangeHandler } label = { this.state.userForm.gender.label } name = { this.state.userForm.name }  />
+            <CustomSelect type = { this.state.userForm.gender.type } elementConfig = { this.state.userForm.gender.elementConfig } value = { this.state.userForm.gender.value } valueChangeHandler = { this.valueChangeHandler } label = { this.state.userForm.gender.label } name = { this.state.userForm.name.name }  />
         </form>
     );
   }
